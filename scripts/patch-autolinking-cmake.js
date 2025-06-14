92236616bb20e86cebf379314f9d40f6d4b2c704
Mr. Hasan <hasanshahriar32@gmail.com>
2025-06-15 03:06:11 +0600

    fix: update CI workflow to copy and patch MMKV CMakeLists.txt and Android-autolinking.cmake

--- scripts/patch-autolinking-cmake.js
+++ scripts/patch-autolinking-cmake.js
@@ -1,5 +1,5 @@
 // scripts/patch-autolinking-cmake.js
-// Patch Android-autolinking.cmake to comment out broken MMKV codegen JNI line
+// Patch Android-autolinking.cmake to comment out broken MMKV codegen JNI line and target_link_libraries for react_codegen_Mmkv
 
 const fs = require('fs');
 const path = require('path');
@@ -11,18 +11,21 @@ const cmakePath = path.join(
 
 if (fs.existsSync(cmakePath)) {
   let content = fs.readFileSync(cmakePath, 'utf8');
-  const mmkvRegex = /^(\s*)add_subdirectory\(([^)]*react-native-mmkv[^)]*codegen[^)]*jni[^)]*)\)/gm;
+  // Patch add_subdirectory for MMKV codegen JNI
+  const mmkvRegex = /^(\s*)add_subdirectory\([^\n]*react-native-mmkv[^\n]*codegen[^\n]*jni[^\n]*\)/gm;
+  // Patch target_link_libraries for react_codegen_Mmkv
+  const linkRegex = /^(\s*)target_link_libraries\(react_codegen_Mmkv[^\n]*\)/gm;
   let patched = false;
-  content = content.replace(mmkvRegex, '$1# add_subdirectory($2)  # patched by patch-autolinking-cmake.js');
-  if (content !== fs.readFileSync(cmakePath, 'utf8')) {
-    fs.writeFileSync(cmakePath, content, 'utf8');
-    patched = true;
+  let newContent = content.replace(mmkvRegex, '$1# add_subdirectory(...)  # patched by patch-autolinking-cmake.js');
+  if (newContent !== content) patched = true;
+  let finalContent = newContent.replace(linkRegex, '$1# target_link_libraries(react_codegen_Mmkv ...)  # patched by patch-autolinking-cmake.js');
+  if (finalContent !== newContent) patched = true;
+  if (patched) {
+    fs.writeFileSync(cmakePath, finalContent, 'utf8');
+    console.log('Patched Android-autolinking.cmake to comment out MMKV codegen JNI and target_link_libraries for react_codegen_Mmkv.');
+  } else {
+    console.log('No MMKV codegen JNI or react_codegen_Mmkv target_link_libraries line found or already patched.');
   }
-  console.log(
-    patched
-      ? 'Patched Android-autolinking.cmake to comment out MMKV codegen JNI.'
-      : 'No MMKV codegen JNI add_subdirectory line found or already patched.'
-  );
 } else {
   console.log('Android-autolinking.cmake not found, skipping patch.');
 }
