f43d67fde7481753f59a16d624b092c09840704b
Mr. Hasan <hasanshahriar32@gmail.com>
2025-06-14 14:47:28 +0600

    feat: update project configuration and dependencies for React Native app

--- App.tsx
+++ App.tsx
@@ -9,6 +9,9 @@ import * as React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
 
 function App() {
   return (
